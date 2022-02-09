51e5fb3de248de2d6422ae803574ba9f740f4192
sexy <test@test.com>
2022-02-10 01:12:22 +0500

    Доработала динамический блок контент, базовый шаблон, шапку сайта (header)

--- aalexmmmm/doramas/views.py
+++ aalexmmmm/doramas/views.py
@@ -6,9 +6,11 @@ from .models import *
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
+
 def index(request):  # Функция представления для главной страницы.
     posts = Doramas.objects.all()
     return render(request, 'doramas/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
 
 def about(request):  # Функция представления для страницы О сайте.
     return render(request, 'doramas/about.html', {'menu': menu, 'title': 'О сайте'})
@@ -20,6 +22,7 @@ def about(request):  # Функция представления для стра
 #     if (int(year) > 2020):
 #         return redirect('home', permanent=True)  # Код для 301 редиректа - перенаправление на главную страницу
 #     return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')  # Функция для обработки исключений
